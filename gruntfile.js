21384e0afffaa633c01a9b39e5de0b908e560d82
Gediminas Backevicius <gediminas.backevicius@gmail.com>
2014-08-07 09:24:52 +0300

    Building procedures has been updated

--- gruntfile.js
+++ gruntfile.js
@@ -154,15 +154,16 @@ module.exports = function (grunt) {
         version: '2.0.1',
         options: {
           specs: './spec/**/*.spec.js',
-          outfile: './SpecRunner.html',
+          outfile: './spec-runner.html',
           keepRunner: true
         }
       },
       'min-spec': {
         src: './<%= pkg.name %>.min.js',
+        version: '2.0.1',
         options: {
           specs: './spec/**/*.spec.js',
-          outfile: './SpecRunner.html',
+          outfile: './spec-runner.html',
           keepRunner: true
         }
       }
@@ -203,10 +204,10 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-jasmine-node');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('default', [
     'jshint:src',
